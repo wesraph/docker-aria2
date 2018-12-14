7557c98af2fa3e97d79c80b5bb1ce03357c2e5ac
Grégoire Delattre <gregoire.delattre@gmail.com>
2018-12-14 17:25:36 +0100

    Add the rpc secrets in the config files

--- run.sh
+++ run.sh
@@ -13,10 +13,19 @@ ARIA2_CONFIG_DIR="/home/aria2"
 
 mkdir -p /downloads/doing /downloads/done
 
+# Update the secret in the index
+encoded_secret=$(echo "$ARIA2_RPC_SECRET" | base64)
+sed -i "s/secret:\"\"/secret:\"$encoded_secret\"/g" /home/aria2/web/index.html
+
+# Add secret to aria
+if ! grep "rpc-secret" /home/aria2/aria2.conf; then
+	echo "rpc-secret=$ARIA2_RPC_SECRET" >> /home/aria2/aria2.conf
+fi
+
 # Mount permissions
 chown -R aria2:aria2 "$ARIA2_CONFIG_DIR"
 chown -R aria2:aria2 /downloads
 
 # Run aria2 as the aria2 user
-su -c "/usr/bin/aria2c --conf-path $ARIA2_CONFIG_DIR/aria2.conf --rpc-secret $ARIA2_RPC_SECRET -D" - aria2
+su -c "/usr/bin/aria2c --conf-path $ARIA2_CONFIG_DIR/aria2.conf -D" - aria2
 exec su -c "/usr/local/bin/caddy -conf /home/aria2/Caddyfile" - aria2
